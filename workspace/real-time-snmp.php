fd8b8481d1fb8a0f8433562b5c44d454f4a6a396
matej <matejpiskac@seznam.cz>
2024-12-06 16:20:18 +0100

    debug

--- workspace/real-time-snmp.php
+++ workspace/real-time-snmp.php
@@ -70,7 +70,7 @@ function getRealTimeArray($type, $ip) {
                 $oid_req = @snmpwalk($ip, $community, $value);
                 $oid_arr = snmpFormat($oid_req, "INTEGER: ");
 
-                $_SESSION["oid"][] = $oid_arr;
+                $_SESSION["oid"] = $oid_arr;
 
                 foreach ($value["id"] as $elemetId => $htmlTemplate) {
                     $htmlResolved = "";
