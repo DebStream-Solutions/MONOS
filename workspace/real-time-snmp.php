58d72188e4c8bb245037946cbd478ea8a3bf4549
matej <matejpiskac@seznam.cz>
2024-12-20 20:48:57 +0100

    debugging - added sql

--- workspace/real-time-snmp.php
+++ workspace/real-time-snmp.php
@@ -2,6 +2,19 @@
 
 session_start();
 
+$servername = "localhost";
+$username = "root";
+$password = "abcdef";
+$dbname = "monos";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 # -- ADDON FUNCTIONS ------
 
 function snmpFormat($snmp_arr, $separator) {
