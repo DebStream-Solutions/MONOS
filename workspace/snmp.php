3a6868eda2e820b7e16dc92b8ab78bef563d185d
Matej <matejpiskac@proton.me>
2025-01-16 14:06:22 +0100

    style of chart button

--- workspace/snmp.php
+++ workspace/snmp.php
@@ -130,7 +130,8 @@ function router($hostIp, $community) {
                         </div>
                         <div class='roll interface'>
                             <div>
-                                <a class='interface-chart-btn-mini' href='".$_SESSION["path"]."&chart-interface={$interfaceId}'>
+                                <a class='interface-chart-btn' href='".$_SESSION["path"]."&chart-interface={$interfaceId}'>
+                                    <span>See in chart</span>
                                     <img src='../icons/pie-chart.png' alt=''>
                                 </a>
                                 <div id='adminStatus{$interfaceId}'>Admin Status: {$if_admin_status_arr[$key]}</div>
