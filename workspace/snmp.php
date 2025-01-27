54c0f1cb7eab788a97d287780bfcf6fdcf61ec50
matej <matejpiskac@seznam.cz>
2025-01-27 22:09:19 +0100

    update msg when offline style

--- workspace/snmp.php
+++ workspace/snmp.php
@@ -88,7 +88,7 @@ function router($hostIp, $community) {
     if ($session->getError()) {
         $generative_content = "Error: " . $session->getError();
     } elseif (ping($hostIp) != true) {
-        $generative_content = "<div style='margin: auto;'>Host is offline (No route to host)</div>";
+        $generative_content = "<div style='margin: auto; text-align: center; max-width: 80vw;'>Host is offline (No route to host)</div>";
     } else {
         foreach ($oid_list as $key => $value) {
             if ($key == "interface") {
@@ -429,7 +429,7 @@ function workstation($hostIp, $community) {
     if ($session->getError()) {
         $generative_content = "Error: " . $session->getError();
     } elseif (ping($hostIp) != true) {
-        $generative_content = "<div style='margin: auto;'>Host is offline (No route to host)</div>";
+        $generative_content = "<div style='margin: auto; text-align: center; max-width: 80vw;'>Host is offline (No route to host)</div>";
     } else {
         foreach ($oid_list as $key => $value) {
             if ($key == "disk") {
